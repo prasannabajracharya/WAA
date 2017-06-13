5d055c991a645066db72f403b25bb19d4d0a3c54
Prasanna <prasanna.bajracharya@gmail.com>
2017-06-13 16:48:21 -0500

    Assignment completed : both cars and books modules are working

--- exercise8_1/src/main/java/cs544/exercise8/NoSuchResourceException.java
+++ exercise8_1/src/main/java/cs544/exercise8/NoSuchResourceException.java
@@ -1,24 +1,24 @@
 package cs544.exercise8;
 
 public class NoSuchResourceException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-	
-	private long resourceId;
-	private String resourceType;
+    private static final long serialVersionUID = 1L;
 
-	public NoSuchResourceException(String resourceType, long resourceId) {
-		super();
-		this.resourceType = resourceType;
-		this.resourceId = resourceId;
-	}
-	
-	public long getResourceId() {
-		return resourceId;
-	}
-	
-	public String getResourceType() {
-		return resourceType;
-	}
+    private long resourceId;
+    private String resourceType;
+
+    public NoSuchResourceException(String resourceType, long resourceId) {
+        super();
+        this.resourceType = resourceType;
+        this.resourceId = resourceId;
+    }
+
+    public long getResourceId() {
+        return resourceId;
+    }
+
+    public String getResourceType() {
+        return resourceType;
+    }
 
 }
 
--- exercise8_1/src/main/java/cs544/sample/ICarDao.java
+++ exercise8_1/src/main/java/cs544/sample/ICarDao.java
@@ -4,14 +4,14 @@ import java.util.List;
 
 public interface ICarDao {
 
-	public abstract List<Car> getAll();
+    public abstract List<Car> getAll();
 
-	public abstract void add(Car car);
+    public abstract void add(Car car);
 
-	public abstract Car get(int id);
+    public abstract Car get(int id);
 
-	public abstract void update(int carId, Car car);
+    public abstract void update(int carId, Car car);
 
-	public abstract void delete(int carId);
+    public abstract void delete(int carId);
 
 }
